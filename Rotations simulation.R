0553f6830ab411d00de5ee985b9754725bd94144
Ian Hastings <ian.hastings@lstmed.ac.uk>
2017-03-10 17:49:50 +0000

    updating array structure.. not yet completed

--- Rotations simulation.R
+++ Rotations simulation.R
@@ -1,7 +1,7 @@
 #this is to simulate the effect of rotations on teh spread of resistnce
-#Written by Ian Hastings, but hopefully Andy South will erad it, give it a D-, and make the code efficient
+#Written by Ian Hastings, but hopefully Andy South will read it, give it a D-, and make the code efficient
 
-#This can run any number of insecticides/loci but at prseentm, input will only allow a maximumum of 5
+#This can run any number of insecticides/loci but at present, input will only allow a maximumum of 5
 
 
 # <<<<<<<<<<<<<<<first up are user-defined parameters>>>>>>>>>>>
@@ -12,17 +12,23 @@ max_no_gens=500 #the maximum number of mosquito generations to run the simulatio
 no_insectides=3 #the number of insecticides (and hence loci) in the simuation MAX IS 5<<<
 
 #now set up some arrays to hold data. Can make some dimensions the number of insecticides but hard-code as 5 meanwhile
-#because diretly write to these arrays below and need at least 5. 
-RAF_male_intervention <- array(0, dim=c(5, max_no_generations)); #resistance allele freq in intervention site
-RAF_female_intervention <- array(0, dim=c(5, max_no_generations));
-RAF_male_refugia <- array(0, dim=c(5, max_no_generations)); #resistance allele freq in refugia
-RAF_female_refugia <- array(0, dim=c(5, max_no_generations));
-alpha_male_high <- array(0, dim=5); alpha_male_low <- array(0, dim=5); alpha_male_none <- array(0, dim=5);
-alpha_female_high <- array(0, dim=5); alpha_female_low <- array(0, dim=5); alpha_female_none <- array(0, dim=5);
-z_rr <- array(0, dim=5); h_rr <- array(0, 5);
-w_ss_none <- array(0, dim=5);w_ss_low <- array(0, dim=5);w_ss_high <- array(0, dim=5);
-w_rs_none <- array(0, dim=5);w_rs_low <- array(0, dim=5);w_rs_high <- array(0, dim=5);
-w_rr_none <- array(0, dim=5);w_rr_low <- array(0, dim=5);w_rr_high <- array(0, dim=5);
+#because diretly write to these arrays below and need at least 5.
+#Note that the fisrt index always refers to teh inesesctide number
+
+array_named <- function(...)  {  
+array(0, dim = lengths(list(...)), dimnames = list(...))  
+ }  
+
+
+RAF <- array_named(insecticide=1:5, sex=c('male','female'), site=c('intervention','refugia'), gen=1:max_no_gens)
+exposure <- array_named(insecticide=1:5, sex=c('male','female'), amount=c('none','low', 'high'))
+fitness <- array_named(insecticide=1:5, genotype=c('SS','SR', 'RR'), amount=c('none','low', 'high'))
+
+RAF[1, 'male', 'intervention', 1]=0.005
+
+test_value=RAF[1, 'male', 'intervention',1]
+
+
 
 #now resume entering data
 migration_rate=0.01 # migration rate into and out-of the treated area. It is the proportion of the treated population that migrates. We assume that immigration=emigration.
@@ -30,36 +36,43 @@ prop_migrants_male=0.5 # the proportion of migrants that are male
 coverage=0.8; # "coverage" of the intervention is defined as the proportion of mosquitoes that are covered by the intervention (and 1-C is the proportion of the population in the untreated refugia).
 
 freq_rotation=10 #frequency of rotation (in generations) NB if set to zero mean RwR i.e. rotate when resistant
-rotataion_criterion=0.5 #resistant allele frequency that triggers a RwR change.
+rotation_criterion=0.5 #resistant allele frequency that triggers a RwR change.
 
 #inital resistance allele frequency (i.e. generation 1) in the intervention and refugia
-RAF_male_intervention[1,1]=0.001;RAF_female_intervention[1,1]=0.001; RAF_male_refugia[1,1]=0.001; RAF_female_refugia[1,1]=0.001 #locus 1
-RAF_male_intervention[2,1]=0.001;RAF_female_intervention[2,1]=0.001; RAF_male_refugia[2,1]=0.001; RAF_female_refugia[2,1]=0.001
-RAF_male_intervention[3,1]=0.001;RAF_female_intervention[3,1]=0.001; RAF_male_refugia[3,1]=0.001; RAF_female_refugia[3,1]=0.001
-RAF_male_intervention[4,1]=0.001;RAF_female_intervention[4,1]=0.001; RAF_male_refugia[4,1]=0.001; RAF_female_refugia[4,1]=0.001
-RAF_male_intervention[5,1]=0.001;RAF_female_intervention[5,1]=0.001; RAF_male_refugia[5,1]=0.001; RAF_female_refugia[5,1]=0.001
+#locus 1>>
+RAF[1, 'male', 'intervention',1]=0.001;  RAF[1, 'female', 'intervention',1]=0.001;
+RAF[1, 'male', 'refugia',1]=0.001;       RAF[1, 'female', 'refugia',1]=0.001
+#locus 2>>
+RAF[2, 'male', 'intervention',1]=0.001;   RAF[2, 'female', 'intervention',1]=0.001;
+RAF[2, 'male', 'refugia',1]=0.001;        RAF[2, 'female', 'refugia',1]=0.001
+#locus 3>>
+RAF[3, 'male', 'intervention',1]=0.001;  RAF[3, 'female', 'intervention',1]=0.001;
+RAF[3, 'male', 'refugia',1]=0.001;       RAF[3, 'female', 'refugia',1]=0.001
+#locus 4>>
+RAF[4, 'male', 'intervention',1]=0.001;  RAF[4, 'female', 'intervention',1]=0.001;
+RAF[4, 'male', 'refugia',1]=0.001;       RAF[4, 'female', 'refugia',1]=0.001
+#locus 5>>
+RAF[5, 'male', 'intervention',1]=0.001; RAF[5, 'female', 'intervention',1]=0.001;
+RAF[5, 'male', 'refugia',1]=0.001;      RAF[5, 'female', 'refugia',1]=0.001
+
+
+
 
 #exposure patterns for insecticide 1
-alpha_male_high[1]=0.3; alpha_male_low[1]=0.7; alpha_male_none[1]=1-alpha_male_high-alpha_male_low
-alpha_female_high[1]=0.3; alpha_female_low[1]=0.7; alpha_female_none[1]=1-alpha_female_high-alpha_female_low
-#if(alpha_male_none[1]<0 or alpha_female_none[1]<0) print_error   #error because exposuer to low and high must exceed one
+exposure[1, 'male', 'low'] =0.1; exposure[1, 'male', 'high'] =0.1;
+exposure[1, 'male', 'none'] =1-exposure[1, 'male', 'low']-exposure[1, 'male', 'high']; 
+exposure[1, 'female', 'low'] =0.1; exposure[1, 'female', 'high'] =0.1;
+exposure[1, 'female', 'none'] =1-exposure[1, 'female', 'low']-exposure[1, 'female', 'high']; 
 
-#other insectides: #we will initially assume these are the same as for insecticide 1; can add code later to over-ride this if required
-alpha_male_high[2]=alpha_male_high[1]; alpha_male_low[2]=alpha_male_low[1]; alpha_male_none[2]=alpha_male_none[1];  
-alpha_male_high[3]=alpha_male_high[1]; alpha_male_low[3]=alpha_male_low[1]; alpha_male_none[3]=alpha_male_none[1];
-alpha_male_high[4]=alpha_male_high[1]; alpha_male_low[4]=alpha_male_low[1]; alpha_male_none[4]=alpha_male_none[1];
-alpha_male_high[5]=alpha_male_high[1]; alpha_male_low[5]=alpha_male_low[1]; alpha_male_none[5]=alpha_male_none[1];
-alpha_female_high[2]=alpha_female_high[1]; alpha_female_low[2]=alpha_female_low[1]; alpha_female_none[2]=alpha_female_none[1];
-alpha_female_high[3]=alpha_female_high[1]; alpha_female_low[3]=alpha_female_low[1]; alpha_female_none[3]=alpha_female_none[1];
-alpha_female_high[4]=alpha_female_high[1]; alpha_female_low[4]=alpha_female_low[1]; alpha_female_none[4]=alpha_female_none[1];
-alpha_female_high[5]=alpha_female_high[1]; alpha_female_low[5]=alpha_female_low[1]; alpha_female_none[5]=alpha_female_none[1];
+
+#other insectides: we will initially assume these are the same as for insecticide 1
+#>>>TO do check no expsoure(none) <1
 
 #genetic data for locus 1
-z_rr[1]=0.9; #natural selection against the RR genotype
-hz_rr[1]=0.9; # the dominance of natural selection against the RR genotype
-w_ss_none[1]=1; w_rs_none[1]=1-hz_rr[1]*z_rr[1];w_rs_none[1]=1-hz_rr[1]*z_rr[1];
-w_ss_low[1]=0.1
-w_ss_high[1]=0.0
+fitness[1, 'SS', 'none']=0.3; fitness[1, 'SS', 'low']=0.3; fitness[1, 'SS', 'high']=0.3;
+fitness[1, 'SR', 'none']=0.3; fitness[1, 'SR', 'low']=0.3; fitness[1, 'SR', 'high']=0.3;
+fitness[1, 'RR', 'none']=0.3; fitness[1, 'RR', 'low']=0.3; fitness[1, 'RR', 'high']=0.3;
+
 
 # <<<<<<<<<<<<< end of user-defined variable >>>>>>>>>>>>>>>>>>>>>>>>
 
@@ -68,6 +81,7 @@ w_ss_high[1]=0.0
 #>>>>>>>> now to run the simulations <<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 current_insectide=1 #usually start the rotation sequence at #1 but can specify any one start
+generation_count=0;
 
 for(gen in 2:max_no_generations){    #start at generation 2 because generation 1 holds the user-defined initial allele frequenciess
   
@@ -81,9 +95,10 @@ if(freq_rotation==0){  #i.e.  its a RwR policy:
     #to avoid switching on a implauisbly short timescale.
   }  
  else{ #if freq_rotation>0 then its a routine, periodic rotation
- if (gen/freq_rotation)is an integer { #i.e. if its a multiple of ferauncy of rotation i.e. time to chnage   
+ if (generation_count==freq_rotation) { #i.e. its time to rotate   
    if(current_insecticide=no_insecticides) current_insecticide=1 #restart the sequence of rotation
-   else current_insecticide=current_insecticide+1 #else just move to the next insectide
+   else current_insecticide=current_insecticide+1  #else just move to the next insectide
+   generation_count=0;
    #Need a check in case they all exceed the critical frequency in whichh case we stop the simulation 
  } 
   
@@ -92,16 +107,18 @@ for(insectide in 1:no_insecticides){
 
 #first the intervention site  
  if(insecticide=current_insecticide){ #i.e. insecticide selection taking place
-   temp_coeff_1=(RAF_male_intervention[insecticide,gen-1]*(1-RAF_female_intervention[insecticide,gen-1])+
-                               RAF_female_intervention[insecticide,gen-1]*(1-RAF_male_intervention[insecticide,gen-1]))*0.5
+   temp_coeff_1=(RAF[insecticide, 'male', 'intervention',gen-1]*(1-RAF[insecticide, 'female', 'intervention',gen-1])+
+                  RAF[insecticide, 'female', 'intervention',gen-1]*(1-RAF[insectide, 'male','intervention',gen-1]))*0.5
+ 
    
-   temp_coeff_2=alpha_male_none[insectide]*w_rs_none[insectide]+
-     alpha_male_low[insectide]*w_rs_low[insectide]+
-     alpha_male_high[insectide]*w_rs_low[insectide]
+   temp_coeff_2=exposure[insecticide, 'male', 'none']*fitness[insectide, 'SR', 'none']+
+     exposure[insectide, 'male', 'low']*fitness[insectide, 'SR', 'low']+
+     fitness[insectide, 'male', 'high']*fitness[insectide, 'SR', 'high']
     
-   temp_coeff_3=alpha_female_none[insectide]*w_rs_none[insectide]+
-                   alpha_female_low[insectide]*w_rs_low[insectide]+
-                   alpha_female_high[insectide]*w_rs_low[insectide]
+   temp_coeff_3=exposure[insecticide, 'female', 'none']*fitness[insectide, 'SR', 'none']+
+     exposure[insectide, 'female', 'low']*fitness[insectide, 'SR', 'low']+
+     fitness[insectide, 'female', 'high']*fitness[insectide, 'SR', 'high']
+   
    
 #first the male resistant alleles>>
   temp_term=alpha_male_none[insectide]*w_rr_none[insectide]+
@@ -195,7 +212,8 @@ for(insectide in 1:no_insecticides){
    
   
   
-  
+generation_count=generation_count+1;
+
 } #end of cycle running the gens up to max_no_generations
   
   
