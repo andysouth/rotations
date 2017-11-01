d1606a217ac6b048c12e4425118d747f7ace5153
AndySouth <southandy@gmail.com>
2017-11-01 22:27:44 +0000

    improve plot legends

--- R/rot_plots.r
+++ R/rot_plots.r
@@ -16,7 +16,7 @@
 #' 
 rot_plot_resistance <- function(df_res2,
                                 plot_refuge = TRUE,
-                                logy = TRUE) {
+                                logy = FALSE) {
   
   # column names of input dataframe
   # "generation"  "insecticide"     "resist_gene"  "active_or_refuge" "resistance"
@@ -43,16 +43,16 @@ rot_plot_resistance <- function(df_res2,
     #add insecticide use indication
     #geom_line( aes_string(x='generation',y='i_in_use'), colour='green4', lwd=2) +
     #trying to get 2nd legend (this puts i_in_use in middle OK start)
-    geom_line( aes_string(x='generation',y='i_in_use', colour=factor('i_in_use')), lwd=2) +
-    #the two below don't work to remove title
-    #guide_legend(title=NULL) +
-    #scale_fill_discrete(guide = guide_legend(title = NULL)) +
+    #geom_line( aes_string(x='generation',y='i_in_use', colour=factor('i_in_use')), lwd=2) +
+    
     #annotate("text", x = 0, y = 1.15, label = "deployment", size = 2.5, hjust='left') +
     #scale_y_continuous(breaks=c(0,0.2,0.4,0.6,0.8,1)) +
     #scale_y_continuous(breaks=c(0,0.25,0.5,0.75,1)) +
     #log axis scale to make clearer what's happening at low values
     #scale_y_continuous(trans='log10', labels = scales::comma) +
-    #theme_bw()
+    geom_line( aes_string(x='generation',y='i_in_use',linetype=factor('i_in_use')), colour='green2', lwd=2) +
+    scale_linetype_manual("insecticide in use", values=rep(1,4),
+      guide=guide_legend(keywidth = 5, label=FALSE, override.aes = list(colour=c("green2"),labels=c("insecticide")))) +
     theme_minimal()
   
   if (logy) gg <- gg + scale_y_continuous(trans='log10', 
