074ab4d42ec90208724cea025aa61bf6cbb47060
AndySouth <southandy@gmail.com>
2017-11-21 15:23:50 +0000

    migration tests working

--- tests/testthat/test_rot_migrate.r
+++ tests/testthat/test_rot_migrate.r
@@ -8,36 +8,38 @@ context("migration")
 # test that tot freqs for sites are same before as after
 # maybe test that sum of whole array before and after is the same
 
-RAF2 <- set_start_freqs(n=3, max_gen=1, freqs=c(0.1,0.2,0.3))
+RAF2 <- set_start_freqs(max_gen=1, freqs=c(0.1,0.2,0.3))
 # set freqs in refugia to 0 to see effects of migration
 RAF2[,,"refugia",] <- 0
+# set m' to 0 to check that mf not getting mixed up
+RAF2[,"m",,] <- 0
+
 bef <- RAF2[,,,1]
 aft <- rot_migrate(bef, migration = 0.1, coverage = 0.5)
 
-sum(bef) == sum(aft)
+#best way to compare by eye
+#raf_get(bef,asdf=TRUE)
+#raf_get(aft,asdf=TRUE)
 
-sum(bef[,'m',]) == sum(aft[,'m',])
-# insecticide1
-sum(bef[1,,]) == sum(aft[1,,])
 
-# expect that if an insecticide has freq 0 in both before 
+test_that("sum of frequencies is same before & after migration ...", {
+  
+  expect_equal( sum(bef), sum(aft) )
+  
+})
 
-test_that("migration works for m & f ...", {
+test_that("sum of frequencies for each insecticide is same before & after migration ...", {
   
-  RAF2 <- set_start_freqs(n=3, max_gen=1, freqs=c(0.1,0.2,0.3))
+  for(i in 1:dim(bef)['insecticide'])
+  {
+    expect_equal( sum(raf_get(bef,insecticide=i)), sum(raf_get(aft,insecticide=i)) )    
+  }
+})
+
+test_that("sum of frequencies by sex is same before & after migration ...", {
   
-  # set freqs in refugia to 0 to see effects of migration
-  RAF2[,,"refugia",] <- 0
-  
-  RAF1gen <- rot_migrate(RAF2[,,,1], migration = 0.1, coverage = 0.5)
-  
-  rot_migrate(RAF2[,,,1], migration = 0.1, coverage = 0.5, verbose = TRUE)
-  
-  #try setting m to 0 just to test
-  RAF2[,"m",,] <- 0  
-  
-  #expect ...
-  #expect_equal( a_fitnic['RR1','RR2','A','B'], 1 )
-  
-  
+  for(sex in c('m','f'))
+  {
+    expect_equal( sum(raf_get(bef,sex=sex)), sum(raf_get(aft,sex=sex)) )    
+  }
 })
