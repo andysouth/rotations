6117e00934961e21ae0f0a3988e14123666fe917
AndySouth <southandy@gmail.com>
2017-11-21 10:41:00 +0000

    package file to help with imports

---
+++ R/rotations.r
@@ -0,0 +1,12 @@
+#' Simulating the Evolution of Insecticide Resistance under Rotations
+#'
+#' 
+#' @import ggplot2
+#' @importFrom dplyr group_by summarise %>%
+#' @importFrom grDevices rgb
+#' @importFrom graphics plot
+#' @importFrom stats filter
+#' 
+#' @docType package
+#' @name rotations
+NULL
