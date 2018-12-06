5e4fd17a7af467732a1af4e7758e8e8a1b6b7242
AndySouth <southandy@gmail.com>
2018-12-06 11:00:09 +0000

    tidy

--- inst/documents/sensi_an_rotations_trimmed_4_jim.r
+++ inst/documents/sensi_an_rotations_trimmed_4_jim.r
@@ -9,23 +9,20 @@ require(devtools)
 #install_github('AndySouth/rotations')  
 require(rotations)
 
-
-# linputs is a list of inputs for one scenario
-# run_rot() runs a single scenario
+# linputs       a list of inputs for one scenario
+# run_rot()     runs a single scenario
+# dfres         detailed results from one scenario
+# df_res_all    summary results one row per scenario across all scenarios
 
 # example for 1 scenario
 # linputs <- list(max_gen = 50, n_insecticides = 2)
 # do.call(run_rot, linputs)
-# dfinputs <- as.data.frame(linputs)
 
 # set number of scenarios
-n_scenarios     <- 2 #000 
+n_scenarios     <- 3 #000 
 
 # set random seed so that results can be reproduced
 set.seed(2)
-max_gen         <- 500 #maximum generations in a scenario
-
-
 
 # create a blank dataframe to contain results of all scenarios 
 df_res_all <- data.frame()
@@ -41,30 +38,20 @@ for(i in 1:n_scenarios)
   ## constant inputs ##
   linputs$coverage <- 1
   linputs$migration <- 0
-  linputs$max_gen <- max_gen
-  
-  linputs$min_rwr_interval <- 10
-  linputs$min_gens_switch_back <- 1
-  linputs$no_r_below_start <- FALSE 
-  linputs$no_r_below_mut <- FALSE 
+  linputs$max_gen <- 300
   linputs$plot <-     FALSE
 
   ## variable inputs ##
   linputs$cost <-           runif(1, min=0,   max=0.1) 
-  linputs$expo_hi <-        runif(1, min=0.4, max=0.9)  #min=0.1, max=0.9
-  linputs$male_expo_prop <- runif(1, min=0,   max=1)
   linputs$eff <-            runif(1, min=0.5, max=1) # min=0.3, max=1
-  linputs$rr  <-            runif(1, min=0.1, max=0.9)
-  linputs$dom_sel <-        runif(1, min=0,   max=1)
-  linputs$dom_cos <-        runif(1, min=0,   max=1) 
-  #TODO make this lognormal to get lower frequencies too
-  linputs$start_freqs <-    runif(1, min=0.005,   max=0.1) #note not log yet   
   linputs$n_insecticides <- sample( 2:5, 1 )  #beware set replace=T if more than 1
   
   #to compare sequence to rotation run all the scenarios above for both seq & rot 
   for(rot_or_not in 0:1)
-  {  
-    #to get rot_int of 0 every other run
+  { 
+    #sequence:rot_interval=0 
+    #rotation:rot_interval>0
+    #to get rot_interval of 0 every other run
     if (rot_or_not == 0) linputs$rot_interval <- 0
     else   #random rot interval integer
         linputs$rot_interval <-   sample( 5:50, 1 )  #beware set replace=T if > 1
@@ -76,29 +63,17 @@ for(i in 1:n_scenarios)
     # summarise results per scenario 
     # (number of generations under the resistance threshold)
     # this is also calc at end of run_rot() here is probably better
-    
     res <- rotations::gens_under_thresh(dfres)
     
-    # to plot results for checking
-    if ( linputs$rot_interval == 0 )
-    {
-      gens_seq <- as.numeric(res)
-    } else
-    {
-      gens_rot <- as.numeric(res)
-    } 
+    # for saving of results later
+    if ( linputs$rot_interval == 0 ) { gens_seq <- as.numeric(res)
+    }  else{                           gens_rot <- as.numeric(res) } 
 
-
-  # later I may want to concat all dfres into one object 
-  # (add a column for scenario num) to save for later analysis
-  # e.g. if I wanted to calculate average resistance frequencies over time
+  # later I may want to also concat all dfres into one object for more detailed analysis
     
   } #end rot_or_not loop    
    
   # make an output row for each scenario with inputs, gens_rot, gens_seq
-  # need to make sure I retain the rot_interval for rot
-
-  # add scenario id column onto results
   df_in_out <- as.data.frame(c(linputs, gens_rot=gens_rot, gens_seq=gens_seq, id=i))
   df_in_out$rot_minus_seq <- df_in_out$gens_rot - df_in_out$gens_seq
   
